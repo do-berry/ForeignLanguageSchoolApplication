de23d20e081ec6919f648544b2bd1ed38a0560e6
Dominika <dominikadolik6@gmail.com>
2020-10-24 15:19:55 +0200

    adjustments: sonar fixes

--- fls_backend/payment/models.py
+++ fls_backend/payment/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 from app.models import Person
-from school.models import Language, Group
 
 
 class Payment(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    # group = models.ForeignKey(Group, on_delete=models.CASCADE, default=0)
     description = models.TextField(default="")
     to_pay = models.FloatField(default=0.00)
     paid = models.BooleanField(default=False)
--- fls_backend/payment/serializers.py
+++ fls_backend/payment/serializers.py
@@ -1,15 +1,13 @@
 from rest_framework import serializers, permissions
 
-from app.serializers import FindPersonSerializer, FindLanguageSerializer
+from app.serializers import FindPersonSerializer
 from payment import models
 from payment.models import Payment
-from school.serializers import FindGroupSerializer
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     permission_classes = (permissions.AllowAny,)
     person = FindPersonSerializer()
-    # group = FindGroupSerializer()
 
     class Meta:
         model = models.Payment
@@ -18,10 +16,7 @@ class PaymentSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         person_data = validated_data.pop('person')
         person = FindPersonSerializer.find(FindPersonSerializer(), person_data)
-        # group_data = validated_data.pop('group')
-        #group = FindGroupSerializer.find(FindGroupSerializer(), group_data)
         payment, created = Payment.objects.update_or_create(person=person,
-                                                            # group=group,
                                                             description=validated_data.pop('description'),
                                                             to_pay=validated_data.pop('to_pay'),
                                                             paid=validated_data.pop('paid'))
