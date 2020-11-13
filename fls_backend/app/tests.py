217aa1c404c760f36f6ef1c2fce6e01fd7ee4e0f
Dominika <dominikadolik6@gmail.com>
2020-11-13 21:08:50 +0100

    adjustments: sample test cases

--- fls_backend/app/tests.py
+++ fls_backend/app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app.models import User, Person
+
+
+class PersonTestCase(TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.user = User.objects.create(username="username", password="password", user_type="STUDENT")
+        cls.person = Person.objects.create(name="test", surname="test", user=cls.user, mobile_number="123456789",
+                                           address="test test")
+
+    def test_information_fields(self):
+        self.assertIsInstance(self.person.name, str)
+        self.assertIsInstance(self.person.surname, str)
+        self.assertIsInstance(self.person.mobile_number, str)
+        self.assertIsInstance(self.person.address, str)
+
+    def test_user_field(self):
+        self.assertIsInstance(self.person.user, User)
