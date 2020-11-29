6340e4e178fcf355c9a46a2b0a5638cb9cd6da64
Dominika <dominikadolik6@gmail.com>
2020-11-29 21:17:51 +0100

    VERY IMPORTANT FIX

--- fls_frontend/src/groupdetails/MarksForGroup.js
+++ fls_frontend/src/groupdetails/MarksForGroup.js
@@ -1,13 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import {TableRowForStudent} from "./TableRowForStudent";
+import React, {useContext, useEffect, useState} from 'react';
 import {TableRowForGroup} from "./TableRowForGroup";
 import {AddNewMark} from "./AddNewMark";
+import './GroupMarks.css';
+import {Button} from "react-bootstrap";
+import {AllDatesContext} from "../lessonlist/AllDatesContext";
 
 export const MarksForGroup = (props) => {
     const [studentsMarks, setStudentsMarks] = useState([]);
+    const [marks, setMarks] = useState([]);
+    const [lessonCount, setLessonCount] = useState(0);
+    const [lesson, setLesson] = useState(0);
+    const [dates, setDates] = useContext(AllDatesContext);
+    const [filtered, setFiltered] = useState([]);
+    const [notFiltered, setNotFiltered] = useState([]);
 
     useEffect(() => {
         let tmp = [];
+        let m = [];
 
         fetch('http://127.0.0.1:8000/school/group/get_marks_by_group', {
             method: 'post',
@@ -24,10 +33,32 @@ export const MarksForGroup = (props) => {
                     return tmp.push(value);
                 });
                 setStudentsMarks(tmp);
-                console.log(tmp);
+                Object.entries(response).map(([key, value]) => {
+                    return m.push(value.marks);
+                });
+                setMarks(m);
+                setNotFiltered(tmp);
+                setFiltered(tmp);
+                setLessonCount(dates.length);
             });
 
     }, []);
+
+    function setFilter() {
+        if (lesson === "default") {
+            setFiltered(studentsMarks);
+        } else {
+            setFiltered(studentsMarks);
+            let a = marks;
+            let arr = studentsMarks.slice();
+            var counter = -1;
+            arr.map(student => {
+                counter++;
+                student.marks = a[counter].filter(mark => mark.lesson.toString() === lesson.toString())
+            });
+            setFiltered(arr);
+        }
+    }
 
     return (
         <div>
@@ -35,14 +66,28 @@ export const MarksForGroup = (props) => {
             <br/>
             <AddNewMark group={props.group}/>
             <br/>
+            <div id='date' className='form-inline'>
+                <select className="form-control"
+                        id='date'
+                        value={lesson}
+                        onChange={e => setLesson(e.target.value)}>
+                    <option value={"default"} selected>Wybierz numer lekcji</option>
+                    {Array.from({length: lessonCount}, (_, key) => {
+                        let val = key + 1;
+                        return <option value={val}>{val}</option>;
+                    })}
+                </select>{'   '}
+                <Button id='filter' bsStyle="info" onClick={setFilter}>Filtruj</Button>
+            </div>
+            <br/>
             <table>
-                <tr>
-                    <th>l.p.</th>
-                    <th>Imię</th>
-                    <th>Nazwisko</th>
-                    <th>Ocena</th>
+                <tr id='mytr'>
+                    <th id='myth'>l.p.</th>
+                    <th id='myth'>Imię</th>
+                    <th id='myth'>Nazwisko</th>
+                    <th id='notmyth'>Ocena</th>
                 </tr>
-                {studentsMarks.map((item, i) => <TableRowForGroup item={item} index={i}/>)}
+                {filtered.map((item, i) => <TableRowForGroup item={item} index={i}/>)}
             </table>
         </div>
     );
--- fls_frontend/src/groupdetails/TableRowForGroupMarks.js
+++ fls_frontend/src/groupdetails/TableRowForGroupMarks.js
@@ -5,7 +5,8 @@ export const TableRowForGroupMarks = (props) => {
     function formatText() {
         let result = "";
         for (let i = 0; i < props.marks.length; i++) {
-            let tmp = props.marks[i].value + " - " + props.marks[i].description + "\n";
+            let tmp = props.marks[i].value + " (" + props.marks[i].description + ", wyst.: " +
+                props.marks[i].date + ", lekcja: " + props.marks[i].lesson + ")\n";
             result += tmp;
         }
         return result;
